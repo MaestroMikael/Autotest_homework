0c1dd6c2ccef7bf2afe3a111cfff6d722c89c4d1
MaestroMikael <153710935+MaestroMikael@users.noreply.github.com>
2024-02-01 23:08:14 +0300

    jacoco update

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,12 @@
 
-package ru.netology
+//package ru.netology
 import kotlin.math.max
 
 const val ERROR_CARD = -1
 const val ERROR_CARDLIMIT = -2
 
 fun main() {
-    // println(commissionCalc("VKpay",1000,11000))}
-}
+     println(commissionCalc("VKpay",1000,11000))}
     fun commissionCalc(cardType: String, previousPay: Int, amount: Int): Int {
         return when (cardType) {
             "Maestro", "Mastercard" -> if (previousPay + amount <= 600_000 && amount <= 75_000) {
@@ -22,7 +21,7 @@ fun main() {
                 ERROR_CARDLIMIT
             }
 
-            "VKpay" -> if (previousPay + amount <= 15000 && amount <= 40000) {
+            "VKpay" -> if (previousPay + amount <= 40000 && amount <= 15000) {
                 0
             } else {
                 ERROR_CARDLIMIT
--- src/test/kotlin/CommissionTest.kt
+++ src/test/kotlin/CommissionTest.kt
@@ -1,6 +1,6 @@
 import org.junit.Test
 import org.junit.Assert.assertEquals
-import  ru.netology.commissionCalc
+//import  ru.netology.commissionCalc
 
 class TestCommission {
     @Test
@@ -11,7 +11,22 @@ class TestCommission {
         val result = commissionCalc(cardType = type, previousPay = prev, amount = ammount)
         assertEquals(380, result)
     }
-
+    @Test
+    fun maestroCommisionLimitMount() {
+        val type = "Maestro"
+        val prev = 0
+        val ammount = 6000000
+        val result = commissionCalc(cardType = type, previousPay = prev, amount = ammount)
+        assertEquals(-2, result)
+    }
+    @Test
+    fun maestroCommisionLimitDay() {
+        val type = "Maestro"
+        val prev = 0
+        val ammount = 200000
+        val result = commissionCalc(cardType = type, previousPay = prev, amount = ammount)
+        assertEquals(-2, result)
+    }
     @Test
     fun masterCommision() {
         val type = "Mastercard"
@@ -30,6 +45,22 @@ class TestCommission {
         assertEquals(75, result)
     }
     @Test
+    fun visaCommisionLimitMount() {
+        val type = "Visa"
+        val prev = 0
+        val ammount = 1000000
+        val result = commissionCalc(cardType = type, previousPay = prev, amount = ammount)
+        assertEquals(-2, result)
+    }
+    @Test
+    fun visaCommisionLimitDay() {
+        val type = "Visa"
+        val prev = 0
+        val ammount = 200000
+        val result = commissionCalc(cardType = type, previousPay = prev, amount = ammount)
+        assertEquals(-2, result)
+    }
+    @Test
     fun mirCommision() {
         val type = "Мир"
         val prev = 0
@@ -45,8 +76,29 @@ class TestCommission {
         val result = commissionCalc(cardType = type, previousPay = prev, amount = ammount)
         assertEquals(0, result)
     }
-
-
-
+    @Test
+    fun VKCommisionLimitMount() {
+        val type = "VKpay"
+        val prev = 40000
+        val ammount = 10000
+        val result = commissionCalc(cardType = type, previousPay = prev, amount = ammount)
+        assertEquals(-2, result)
+    }
+    @Test
+    fun VKCommisionLimitDayt() {
+        val type = "VKpay"
+        val prev = 0
+        val ammount = 20000
+        val result = commissionCalc(cardType = type, previousPay = prev, amount = ammount)
+        assertEquals(-2, result)
+    }
+    @Test
+    fun errorCardType() {
+        val type = "VK pay"
+        val prev = 0
+        val ammount = 1000
+        val result = commissionCalc(cardType = type, previousPay = prev, amount = ammount)
+        assertEquals(-1, result)
+    }
 }
 
